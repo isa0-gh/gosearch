0f4724ad26e2f746ac6f9e2efeef86947f0cbd5a
isa0-gh <0isa@proton.me>
2026-06-02 11:34:47 +0300

    test(bing): add manual test binary for Bing scraper

---
+++ cmd/tests/bing/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/isa0-gh/gosearch/internal/scrapers"
+)
+
+func main() {
+	results, err := scrapers.BingSearch("golang", 2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i, r := range results {
+		fmt.Printf("[%d] %s\n    %s\n    %s\n\n", i+1, r.Title, r.URL, r.Snippet)
+	}
+}
